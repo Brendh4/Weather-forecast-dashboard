ab915a056fc493782f51c15248b27b06955418ba
Brendh4 <brendhauchiha@gmail.com>
2024-01-20 14:37:07 +0000

    adds fetch request

--- asset/script.js
+++ asset/script.js
@@ -1,6 +1,24 @@
 var locationInput = document.getElementById("search-input");
 var location = locationInput.value.trim();
 
-var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + location + "&appid=3d7891a22193528f3697763bad757015"
+var apiKey = "&appid=3d7891a22193528f3697763bad757015";
+var apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + location + "&appid=" + apiKey;
 
-console.log(location)
+// Make a GET request to the API     ---> this assistance was provided by Xpert learning assistant as I was stuck
+fetch(apiUrl)
+    .then(function(response) {
+    // Check if the request was successful
+    if (response.ok) {
+        return response.json();
+    } else {
+        throw new Error("Error: " + response.status);
+    }
+})
+.then(function(data) {
+// Process the weather data
+    console.log(data);
+// Display the weather information on the page
+  })
+ .catch(function(error) {
+   console.log(error);
+ });

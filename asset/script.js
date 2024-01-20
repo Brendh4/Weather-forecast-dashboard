0a04632010cb8006b4c4de505d5c3d475652fc04
Brendh4 <brendhauchiha@gmail.com>
2024-01-20 14:31:58 +0000

    uses input value, logs location to console to test

--- asset/script.js
+++ asset/script.js
@@ -1,1 +1,6 @@
-var queryURL = "api.openweathermap.org/data/2.5/forecast?lat=44.34&lon=10.99&appid=3d7891a22193528f3697763bad757015"
+var locationInput = document.getElementById("search-input");
+var location = locationInput.value.trim();
+
+var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + location + "&appid=3d7891a22193528f3697763bad757015"
+
+console.log(location)

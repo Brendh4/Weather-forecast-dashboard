902cda2f6241bd13f7abf9ba73e6461f1249f4ed
Brendh4 <brendhauchiha@gmail.com>
2024-01-24 21:37:44 +0000

    adds todays weather

--- asset/script.js
+++ asset/script.js
@@ -1,24 +1,40 @@
-var locationInput = document.getElementById("search-input");
-var location = locationInput.value.trim();
+document.getElementById("search-form").addEventListener("submit", function (event) {
+    event.preventDefault();
 
-var apiKey = "&appid=3d7891a22193528f3697763bad757015";
-var apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + location + "&appid=" + apiKey;
+    var locationInput = document.getElementById("search-input");
+    var location = locationInput.value.trim();
 
-// Make a GET request to the API     ---> this assistance was provided by Xpert learning assistant as I was stuck
-fetch(apiUrl)
-    .then(function(response) {
-    // Check if the request was successful
-    if (response.ok) {
-        return response.json();
-    } else {
-        throw new Error("Error: " + response.status);
-    }
-})
-.then(function(data) {
-// Process the weather data
-    console.log(data);
-// Display the weather information on the page
-  })
- .catch(function(error) {
-   console.log(error);
- });
+    var apiKey = "3d7891a22193528f3697763bad757015";
+    var apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + location + "&appid=" + apiKey;
+
+    // Make a GET request to the API
+    fetch(apiUrl)
+    .then(function (response) {
+        if (response.ok) {
+            return response.json();
+        } else {
+            throw new Error("Error: " + response.status);
+        }
+    })
+    .then(function (data) {
+    // Process the weather data
+    displayWeather(data);
+    })
+    .catch(function (error) {
+        // Display error message
+        console.log(error);
+        alert("Error. Please try again.");
+    });
+});
+
+function displayWeather(weatherData) {
+    // Display the weather information on the page
+    var cityName = weatherData.name;
+    var temperature = weatherData.main.temp;
+    var description = weatherData.weather[0].description;
+
+    var todaySection = document.getElementById("today");
+    todaySection.innerHTML = "<h2>" + cityName + "</h2>";
+    todaySection.innerHTML += "<p>Temperature: " + (temperature - 273.15).toFixed(2) + " Celsius</p>";
+    todaySection.innerHTML += "<p>Description: " + description + "</p>";
+}
